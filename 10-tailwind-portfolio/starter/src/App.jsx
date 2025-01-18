cf6023640e882db605c50528727edef32210ccc6
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2025-01-18 10:13:48 +0530

    Completed Taliwind basic Project

--- 10-tailwind-portfolio/starter/src/App.jsx
+++ 10-tailwind-portfolio/starter/src/App.jsx
@@ -1,8 +1,19 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 const App = () => {
   return (
-    <h2>App</h2>
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <About />
+      <Projects />
+    </>
   );
 }
 
-export default App
+export default App;
