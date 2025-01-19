59e54986c80a7b249cf4f711e686b66de1136590
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2025-01-19 14:40:00 +0530

    Redux basics project complete

--- 11-redux-toolkit-tutorial/vite-starter/src/App.jsx
+++ 11-redux-toolkit-tutorial/vite-starter/src/App.jsx
@@ -1,4 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import CartContainer from './components/CartContainer.jsx';
+import Navbar from './components/Navbar.jsx';
+import { calculateTotals } from './features/cart/cartSlice.js';
+import { useEffect } from 'react';
+import Modal from './components/Modal.jsx';
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const cartItems = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
