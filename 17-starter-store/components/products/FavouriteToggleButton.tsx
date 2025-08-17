29f937c0ef32f4649122423d434e42478e5660e5
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2025-08-17 09:00:34 +0530

    Favourites Feature updated

--- 17-starter-store/components/products/FavouriteToggleButton.tsx
+++ 17-starter-store/components/products/FavouriteToggleButton.tsx
@@ -1,11 +1,15 @@
-import { FaHeart } from "react-icons/fa";
-import { Button } from "../ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { CardSignInButton } from "../form/Buttons";
+import { fetchFavouriteId } from "@/utils/actions";
+import FavouriteToggleForm from "./FavouriteToggleForm";
 
-function FavouriteToggleButton({ productId }: { productId: string }) {
-  return (
-    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
-      <FaHeart />
-    </Button>
-  );
+async function FavouriteToggleButton({ productId }: { productId: string }) {
+  const { userId } = auth();
+
+  if (!userId) return <CardSignInButton />;
+  
+  const favouriteId = await fetchFavouriteId({ productId });
+
+  return <FavouriteToggleForm favouriteId={favouriteId} productId={productId} />;
 }
 export default FavouriteToggleButton;
